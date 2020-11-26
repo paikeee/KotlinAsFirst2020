b62d5ce13f452a91954c501d6489607e603b57bc
paikeee <serzhik2002@gmail.com>
2020-11-26 12:58:22 +0300

    1

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -170,32 +170,7 @@ fun centerFile(inputName: String, outputName: String) {
  * 8) Если входной файл удовлетворяет требованиям 1-7, то он должен быть в точности идентичен выходному файлу
  */
 fun alignFileByWidth(inputName: String, outputName: String) {
-    val writer = File(outputName).bufferedWriter()
-    var longest = 0
-    for (line in File(inputName).readLines())
-        if (line.length > longest) longest = line.trim().length
-    for (line in File(inputName).readLines()) {
-        val wordsCount = line.trim().split(" ").size
-        if (line.trim().isNotEmpty() && wordsCount > 1) {
-            val words = line.trim().toCharArray()
-            val spaces = (longest - line.trim().length) / (wordsCount - 1)
-            var extraSpaces = (longest - line.trim().length) % (wordsCount - 1)
-            var newLine = ""
-            var k = 0
-            for (word in words) {
-                if (word != ' ') k = 0
-                if (word == ' ' && k == 0) {
-                    newLine += if (extraSpaces > 0)
-                        word + " ".repeat(spaces + 1)
-                    else word + " ".repeat(spaces)
-                    extraSpaces--
-                    k = 1
-                } else newLine += word
-            }
-            writer.appendLine(newLine.trim())
-        } else writer.appendLine(line.trim())
-    }
-    writer.close()
+    TODO()
 }
 
 /**
